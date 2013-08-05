5c252bf7bf7ca4d82572d654ecaa519462db49dd
Keturah Walters <your.ketwalters@gmail.com>
2013-08-05 14:14:01 -0400

    Laser Game

---
+++ window.rb
@@ -0,0 +1,22 @@
+class Window < Gosu::Window
+
+    def initialize
+        super(300,300,false)
+        @player = Player.new(self)
+    end
+   
+    def draw
+      @player.draw
+    end
+        
+        def update
+      if button_down? Gosu::Button::KbLeft
+        @player.move_left
+      end
+            
+            if button_down? Gosu::Button::KbRight
+        @player.move_right
+      end
+    end
+   
+end
