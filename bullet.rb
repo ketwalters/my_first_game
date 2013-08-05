43c2d4f3db7bbe42c486306f26aa38126cb676f3
Keturah Walters <your.ketwalters@gmail.com>
2013-08-05 14:28:41 -0400

    Laser Game

--- bullet.rb
+++ bullet.rb
@@ -1,22 +1,31 @@
 class Bullet
-    attr_reader :x, :y
-
+    attr_reader :x, :y, :alive
+    
     def initialize(window)
         @window = window
         @icon = Gosu::Image.new(@window, 'bullet.png', true)
-        @y = 0
+        @y = -rand(1000)
         @x = rand(@window.width)
+        @alive = true
     end
     
-    def update
+    def update(laser)
         @y = @y + 10
-        if @y > @window.width
+        if @y > @window.height
             @y = 0
             @x = rand(@window.width)    
         end
+        hit_by?(laser)
     end
     
     def draw
-        @icon.draw(@x, @y, 3)
+        @icon.draw(@x, @y, 5)
     end
+    
+    def hit_by?(laser)
+        if Gosu::distance(laser.x, laser.y, @x, @y) < 20
+            @alive = false
+        end
+    end
+    
 end
